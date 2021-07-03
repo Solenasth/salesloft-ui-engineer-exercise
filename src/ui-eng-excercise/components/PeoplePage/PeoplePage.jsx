859cbb6629bc102bde5a942834a5c194ab1f31c0
Kobo <solenasthellion@gmail.com>
2021-07-03 18:17:42 -0500

    PeoplePage draft: - first layout iteration, now renders multiple components. - adds title bar.

--- src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
+++ src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Panel from '../Panel/Panel';
+import TitleBar from '../TitleBar/TitleBar';
+
+import './PeoplePage.css';
 
 const PeoplePage = (
   {
@@ -6,15 +10,13 @@ const PeoplePage = (
   }
 ) => {
   return (
-    <div>
-      <h1>hello world</h1>
-      <h2>How much does it cost per episode Game of Thrones?</h2>
-      <p>
-        The average Game of Thrones episode costs $6 million to make. That's two
-        to three times what a typical network or cable show costs per episode
-        (Breaking Bad episodes cost around $3 million, early Big Bang Theory
-        cost $2 million). (From google's "Im feeling curious")
-      </p>
+    <div className="page">
+      <TitleBar title="People" />
+      <div>
+        <Panel />
+        <Panel />
+        <Panel />
+      </div>
     </div>
   );
 };

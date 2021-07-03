b67d64f011f77a1ac0809f92ac3389ace051f84a
Kobo <solenasthellion@gmail.com>
2021-07-03 15:19:27 -0500

    Initial commit: - adds folder structure for the excercise components. - adds scaffolding for PeoplePage. - renders PeoplePage in App.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React from 'react';
+import PeoplePage from './ui-eng-excercise/components/PeoplePage/PeoplePage';
 
 export const App = () => {
-  return <>{/* YOUR CODE GOES HERE */}</>;
+  return (
+    <>
+      <PeoplePage />
+    </>
+  );
 };

0780f52966f5c1392f616983d8c37e13d5f22893
Kobo <solenasthellion@gmail.com>
2021-07-03 18:16:37 -0500

    Adds a draft panel component & Styling

---
+++ src/ui-eng-excercise/components/Panel/Panel.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Panel.css';
+
+const Panel = (
+  {
+    /* props */
+  }
+) => {
+  return (
+    <div className="panel">
+      <h1>hello world</h1>
+      <h2>How much does it cost per episode Game of Thrones?</h2>
+      <p>
+        The average Game of Thrones episode costs $6 million to make. That's two
+        to three times what a typical network or cable show costs per episode
+        (Breaking Bad episodes cost around $3 million, early Big Bang Theory
+        cost $2 million). (From google's "Im feeling curious")
+      </p>
+    </div>
+  );
+};
+
+export default Panel;

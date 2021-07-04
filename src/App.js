b3338b687a2a1d792d8d0c72d2d3d02735c80894
Kobo <solenasthellion@gmail.com>
2021-07-04 01:11:28 -0500

    Adds mock payload - added in app, so it's received as props on PeoplePage.
    My guess is: the payload is fetched when we navigate to a specific menu
    so this kinda-sorta simulates it being received as a prop.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PeoplePage from './ui-eng-excercise/components/PeoplePage/PeoplePage';
+//mock data
+import mock_payload from './ui-eng-excercise/mockPayload';
 
 export const App = () => {
   return (
     <>
-      <PeoplePage />
+      <PeoplePage payload={mock_payload} />
     </>
   );
 };

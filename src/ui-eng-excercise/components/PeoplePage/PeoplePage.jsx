feddc3725a235f1341a31365cf65a15c7d50bbac
Kobo <solenasthellion@gmail.com>
2021-07-04 01:18:28 -0500

    Adds props to PeoplePage Sends required props to Panel

--- src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
+++ src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
@@ -4,21 +4,24 @@ import TitleBar from '../TitleBar/TitleBar';
 
 import './PeoplePage.css';
 
-const PeoplePage = (
-  {
-    /* props */
-  }
-) => {
+const PeoplePage = ({ payload }) => {
   return (
     <div className="page">
       <TitleBar title="People" />
       <div className="page-layout">
-        <Panel type="profile" />
+        <Panel
+          type="profile"
+          name={payload.display_name}
+          title={payload.title}
+          company_name={payload.person_company_name}
+          company_website={payload.person_company_website}
+        />
         <Panel type="activities" />
         <Panel />
         <Panel />
         <Panel />
       </div>
+      {/* <div>{JSON.stringify(payload)}</div> */}
     </div>
   );
 };

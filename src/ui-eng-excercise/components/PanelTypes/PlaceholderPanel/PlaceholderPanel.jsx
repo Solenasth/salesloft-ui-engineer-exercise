a3d0cfb3d5407b6f4d2fd81806b4bf4624aa2097
Kobo <solenasthellion@gmail.com>
2021-07-05 18:32:17 -0500

    Improves placeholder Panels - this is just a small bit of fun code. it makes heights semi-random. - completely useless asuming we would have a full ui - i just wanted to see diferent sizes witout explicitly setting them.

--- src/ui-eng-excercise/components/PanelTypes/PlaceholderPanel/PlaceholderPanel.jsx
+++ src/ui-eng-excercise/components/PanelTypes/PlaceholderPanel/PlaceholderPanel.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './PlaceholderPanel.css';
 
-const PlaceholderPanel = ({ payload }) => {
-  return <div className="placeholder-panel" />;
+const PlaceholderPanel = () => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const getRandomSizeClass = luck => {
+    switch (luck) {
+      case 0:
+        return 'sm';
+      case 1:
+        return 'md';
+      case 2:
+        return 'l';
+      case 3:
+      default:
+        return 'xl';
+    }
+  };
+  return (
+    <div
+      className={`placeholder-panel ${getRandomSizeClass(getRandomInt(4))}`}
+    />
+  );
 };
 
 export default PlaceholderPanel;

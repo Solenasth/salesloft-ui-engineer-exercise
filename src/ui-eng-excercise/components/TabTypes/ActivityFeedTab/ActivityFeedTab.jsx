d958851818566814e576d121fa1e4e259860b72d
Kobo <solenasthellion@gmail.com>
2021-07-05 13:43:06 -0500

    Improves styles for text in ActivityFeed - this commit also contains a wip useEffect hook i plan to use to fetch

--- src/ui-eng-excercise/components/TabTypes/ActivityFeedTab/ActivityFeedTab.jsx
+++ src/ui-eng-excercise/components/TabTypes/ActivityFeedTab/ActivityFeedTab.jsx
@@ -3,8 +3,17 @@ import './ActivityFeedTab.css';
 import { default as ChevronSmDownIcon } from '../../../../_starter/shared/Icons/ChevronSmDown';
 import { default as PhoneIcon } from '../../../../_starter/shared/Icons/Phone';
 import { default as PlusIcon } from '../../../../_starter/shared/Icons/Plus';
+import EventCard from '../../EventCard';
 
-const ActivityFeedTab = () => {
+// useEffect(() => {
+//   fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(data => {
+//       setPosts(data);
+//     });
+// }, []);
+
+const ActivityFeedTab = ({ activities, upcoming_activities }) => {
   return (
     <div className="activity-feed">
       <div className="activity-feed__top-buttons">
@@ -33,12 +42,19 @@ const ActivityFeedTab = () => {
           </button>
         </div>
       </div>
-      <div className="activity-feed__upcoming-activities">
-        <h1>Upcoming Activities</h1>
-        <p>Once actions are scheduled, they’ll appear here</p>
+      <div className="upcoming-activities">
+        <h1 className="upcoming-activities__title">Upcoming Activities</h1>
+        <p className="upcoming-activities__empty-text">
+          Once actions are scheduled, they’ll appear here
+        </p>
       </div>
-      <div className="activity-feed__past-activities">
-        <h1>Past Activities</h1>
+      <div className="past-activities">
+        <h1 className="past-activities__title">Past Activities</h1>
+        {/* the mocks don't include a text for an empty list, but i wanted to include it for consistency */}
+        <p className="past-activities__empty-text">
+          Once actions are done, they’ll appear here
+        </p>
+        <EventCard />
       </div>
     </div>
   );

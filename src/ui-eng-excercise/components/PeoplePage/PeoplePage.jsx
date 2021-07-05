2e6a4de31392045b7a56bb9a84b84ebcb299e9ae
Kobo <solenasthellion@gmail.com>
2021-07-05 18:34:37 -0500

    Improves grid. - in order for it to look more like "Masonry" w/o the time invested - also witout using external npms or js. - basically i just use grid to have columns now. - each column now has a flex display inside.

--- src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
+++ src/ui-eng-excercise/components/PeoplePage/PeoplePage.jsx
@@ -9,29 +9,33 @@ const PeoplePage = ({ payload }) => {
     <div className="page">
       <TitleBar title="People" />
       <div className="page-layout">
-        <Panel
-          type="profile"
-          name={payload.display_name}
-          title={payload.title}
-          company_name={payload.person_company_name}
-          company_website={payload.person_company_website}
-          linkedin_url={payload.linkedin_url}
-          twitter_handle={payload.twitter_handle}
-          crm_url={payload.crm_url}
-          personal_website={payload.personal_website}
-        />
+        <div className="layout-left-column">
+          <Panel
+            type="profile"
+            name={payload.display_name}
+            title={payload.title}
+            company_name={payload.person_company_name}
+            company_website={payload.person_company_website}
+            linkedin_url={payload.linkedin_url}
+            twitter_handle={payload.twitter_handle}
+            crm_url={payload.crm_url}
+            personal_website={payload.personal_website}
+          />
+          <Panel />
+          <Panel />
+          <Panel />
+        </div>
+        <div className="layout-right-column">
+          <Panel />
+          <Panel />
+          <Panel />
+          <Panel />
+        </div>
         <Panel
           type="activities"
           className="page-layout__main-panel"
           payload={payload}
         />
-        <Panel />
-        <Panel />
-        <Panel />
-        <Panel />
-        <Panel />
-        <Panel />
-        <Panel />
       </div>
     </div>
   );

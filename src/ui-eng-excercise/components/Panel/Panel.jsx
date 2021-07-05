b56e52451be21edac2ef4b2363d24122e120dd1e
Kobo <solenasthellion@gmail.com>
2021-07-04 23:15:16 -0500

    css fixe removes unecesary wrapper div adds more colors

--- src/ui-eng-excercise/components/Panel/Panel.jsx
+++ src/ui-eng-excercise/components/Panel/Panel.jsx
@@ -27,7 +27,9 @@ const Panel = ({ type, ...rest }) => {
     }
   };
   return (
-    <div className={`panel ${rest.className}`}>{renderPanelType(type)}</div>
+    <div className={`panel ${rest.className ? rest.className : ''}`}>
+      {renderPanelType(type)}
+    </div>
   );
 };
 

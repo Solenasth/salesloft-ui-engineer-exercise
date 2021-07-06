47e8884c97f8de88c9887bc68bf87cc907c1b710
Kobo <solenasthellion@gmail.com>
2021-07-05 21:05:57 -0500

    adds dynamic titles to EventCard - adds prop destructuring - adds some helper classes for title stylings

--- src/ui-eng-excercise/components/EventCard/EventCard.jsx
+++ src/ui-eng-excercise/components/EventCard/EventCard.jsx
@@ -8,6 +8,66 @@ import { default as ReplyIcon } from '../../../_starter/shared/Icons/Reply';
 import { default as RocketIcon } from '../../../_starter/shared/Icons/Rocket';
 
 const EventCard = ({ eventData }) => {
+  const { type, created_at, static_data, dynamic_data } = eventData;
+
+  const parseMinutes = minutes => {
+    let hours = minutes / 60;
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return `${rhours}:${rminutes}`;
+  };
+
+  const getDynamicTitle = (type, static_data, dynamic_data) => {
+    switch (type) {
+      case 'voicemail':
+        return (
+          <>
+            <span className="card-dynamic-title--bold">
+              Voicemail Received{' '}
+            </span>
+            <span>{parseMinutes(dynamic_data.voicemail_duration)}</span>
+          </>
+        );
+      case 'call':
+        return (
+          <span className="card-dynamic-title--bold">
+            {static_data.sentiment}
+          </span>
+        );
+      case 'success':
+        return (
+          <span className="card-dynamic-title--bold">Marked as Success</span>
+        );
+      case 'sent_email':
+        return (
+          <span className="card-dynamic-title--highlight">
+            {static_data.subject}
+          </span>
+        );
+      case 'email_reply':
+        return (
+          <>
+            <span className="card-dynamic-title--highlight">
+              {`RE: ${static_data.in_reply_to_subject} `}
+            </span>
+            <span>{static_data.body}</span>
+          </>
+        );
+      case 'added_to_cadence':
+        return (
+          <>
+            <span className="card-dynamic-title--bold">Added to Cadence </span>
+            <span className="card-dynamic-title--highlight">
+              {dynamic_data.cadence_name}
+            </span>
+          </>
+        );
+      default:
+        return 'title not available';
+    }
+  };
+
   const getIcon = type => {
     switch (type) {
       case 'voicemail':
@@ -42,18 +102,21 @@ const EventCard = ({ eventData }) => {
 
   return (
     <div className="event-card">
-      <div className={`event-icon ${eventData.type}`}>
+      <div className={`event-icon ${type}`}>
         <div className="event-icon__background" />
-        <div className="event-icon__icon">{getIcon(eventData.type)}</div>
+        <div className="event-icon__icon">{getIcon(type)}</div>
       </div>
       <div className="card-body">
         <div className="card-body__content">
-          <h1 className="card-dynamic-title">The convergence is upon us</h1>
+          <h1 className="card-dynamic-title">
+            {getDynamicTitle(type, static_data, dynamic_data)}
+            {console.log(dynamic_data)}
+          </h1>
           <p className="card-dynamic-content">neptune was mutated</p>
         </div>
         <div className="card-datetime">
-          <p className="card-datetime__date">{getDate(eventData.created_at)}</p>
-          <p className="card-datetime__time">{getTime(eventData.created_at)}</p>
+          <p className="card-datetime__date">{getDate(created_at)}</p>
+          <p className="card-datetime__time">{getTime(created_at)}</p>
         </div>
       </div>
     </div>
